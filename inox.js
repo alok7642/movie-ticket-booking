142b049ac3949d862481ebecad131678fdfccbe3
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2025-07-03 13:09:34 +0530

    Add files via upload

---
+++ inox.js
@@ -0,0 +1,31 @@
+function BookClick() {
+    // Hide button and show summary
+    document.getElementById("button-container").style.display = "none";
+    document.getElementById("summary-container").style.display = "block";
+
+    // Set summary details
+    document.getElementById("lblmovie").textContent = document.getElementById("lstmovie").value;
+    document.getElementById("lbldate").textContent = document.getElementById("lstdate").value;
+    document.getElementById("lblcinema").textContent = document.getElementById("lstcinema").value;
+    document.getElementById("lbltime").textContent = document.getElementById("lsttime").value;
+
+    // Set poster
+    const movieName = document.getElementById("lstmovie").value;
+    const poster = document.getElementById("imgPoster");
+
+    if (movieName === "SIKANDAR") {
+        poster.src = "../public/images/sikandar.png";
+    } else if (movieName === "BLACK BAG") {
+        poster.src = "../public/images/black-bag.png";
+    } else {
+        poster.src = ""; // Fallback for undefined or empty selection
+    }
+}
+
+function editBooking(){
+        document.getElementById("booking-title").innerHTML = "Modify Booking ";
+        document.getElementById("btnBook").innerHTML = "save";
+        document.getElementById("btnBook").className = "btn btn-success";
+    
+
+}
